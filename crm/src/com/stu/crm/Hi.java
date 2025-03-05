7046a4c06151dee0af3b0b3f3fcdd509755abfeb
Markkk-He <493290838@qq.com>
2025-03-05 09:58:27 +0800

    Hi commit no1

---
+++ crm/src/com/stu/crm/Hi.java
@@ -0,0 +1,11 @@
+package com.stu.crm;
+
+/**
+ * @author Mark He
+ * @version 1.0
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
